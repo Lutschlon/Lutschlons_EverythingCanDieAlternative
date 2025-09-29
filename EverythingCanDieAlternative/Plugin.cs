3a992f5141c5a66a11f1ea716dc04f78e45b8d0d
Lutschlon <157857853+Lutschlon@users.noreply.github.com>
2025-09-30 00:33:52 +0200

    Add files via upload

--- EverythingCanDieAlternative/Plugin.cs
+++ EverythingCanDieAlternative/Plugin.cs
@@ -295,6 +295,6 @@ namespace EverythingCanDieAlternative
     {
         public const string PLUGIN_GUID = "nwnt.EverythingCanDieAlternative";
         public const string PLUGIN_NAME = "EverythingCanDieAlternative";
-        public const string PLUGIN_VERSION = "1.1.66";
+        public const string PLUGIN_VERSION = "1.1.67";
     }
 }
